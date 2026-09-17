4f3444ac2efe47dbc0909ee5a021fcd071da1d43
emadalqudami17-droid <emadalqudami17@gmail.com>
2026-09-17 20:27:52 +0300

    feat: add documents list screen to navigation
    Added DocumentsListScreen to the application.

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from kivy.uix.screenmanager import ScreenManager, SlideTransition
 from kivy.core.window import Window
 
 from utils.constants import (
-    APP_NAME, COLOR_BG, SCREEN_HOME, SCREEN_EDITOR,
+    APP_NAME, COLOR_BG,
+    SCREEN_HOME, SCREEN_EDITOR, SCREEN_DOCUMENTS,
 )
 from screens.home import HomeScreen
 from screens.editor import EditorScreen
+from screens.documents_list import DocumentsListScreen
 
 
 class AcademicWordEditorApp(App):
@@ -17,12 +19,15 @@ class AcademicWordEditorApp(App):
         self.title = APP_NAME
         Window.clearcolor = COLOR_BG
 
-        # Screen Manager
+        # Screen Manager with slide transition
         sm = ScreenManager(transition=SlideTransition(duration=0.2))
 
+        # Register all screens
         sm.add_widget(HomeScreen(name=SCREEN_HOME))
         sm.add_widget(EditorScreen(name=SCREEN_EDITOR))
+        sm.add_widget(DocumentsListScreen(name=SCREEN_DOCUMENTS))
 
+        # Start at home
         sm.current = SCREEN_HOME
 
         return sm
